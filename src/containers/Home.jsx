708d0a288a307fbf23056148547d5b47724e6d92
oeulloa <oeulloa@tigo.com.gt>
2021-07-23 17:24:09 -0600

    Agregar actions y reducers

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,41 +1,38 @@
 import React, { useEffect, useState } from "react";
-
+import { connect } from "react-redux";
 import Search from "../components/Search";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Categories from "../components/Categories";
-
 import useTvShowsApi from "../hooks/useTvShowsApi";
 import "../assets/styles/App.scss";
 
-const API = "http://localhost:3000/initalState";
-
-const Home = () => {
-  const initialState = useTvShowsApi(API);
-  return initialState.length === 0 ? (
-    <h1>Loading...</h1>
-  ) : (
+const Home = ({ myList, trends, originals }) => {
+  return (
     <>
       <Search />
-      {initialState.mylist.length > 0 && (
+      {myList.length > 0 && (
         <Categories title="Mi lista">
           <Carousel>
-            {initialState.mylist.map((item) => (
-              <CarouselItem key={item.id} {...item} />
+            {myList.map((item) => (
+              <CarouselItem 
+              key={item.id} 
+              {...item} 
+              isList />
             ))}
           </Carousel>
         </Categories>
       )}
       <Categories title="Tendencias">
         <Carousel>
-          {initialState.trends.map((item) => (
+          {trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
       </Categories>
       <Categories title="Originales de Platfix">
         <Carousel>
-          {initialState.originals.map((item) => (
+          {originals.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
@@ -44,4 +41,13 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+// export default Home;
+export default connect(mapStateToProps, null)(Home);
